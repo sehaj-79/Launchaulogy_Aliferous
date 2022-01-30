42a4c877a7798067172568c3ca0d3eaf59b3aa61
sehaj-79 <bhasin.seh79@gmail.com>
2022-01-30 18:53:21 +0530

    Aliment Activity Commit

---
+++ app/src/main/java/com/aliferous/launchaulogy_aliferous/AilmentsActivity.java
@@ -0,0 +1,14 @@
+package com.aliferous.launchaulogy_aliferous;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AilmentsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ailments);
+    }
+}
