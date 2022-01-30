e6db9adfe0ba912314547181a366a311941093de
sehaj-79 <bhasin.seh79@gmail.com>
2022-01-30 17:07:45 +0530

    Pose Detetctor Ml

--- app/src/main/java/com/aliferous/launchaulogy_aliferous/LoginActivity.java
+++ app/src/main/java/com/aliferous/launchaulogy_aliferous/LoginActivity.java
@@ -86,7 +86,7 @@ public class LoginActivity extends AppCompatActivity {
                                 public void onComplete(@NonNull Task<AuthResult> task) {
                                     if (task.isSuccessful()){
                                         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-                                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
                                         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
                                         startActivity(intent);
                                         finish();
