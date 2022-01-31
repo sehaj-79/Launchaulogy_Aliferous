14abb2bcaba36e1f14a7e5ba2840c40029bebedb
Shikhar Kumar <shikharkumar009@gmail.com>
2022-01-31 05:37:58 +0530

    Splash Screen

--- app/src/main/java/com/aliferous/launchaulogy_aliferous/HomeActivity.java
+++ app/src/main/java/com/aliferous/launchaulogy_aliferous/HomeActivity.java
@@ -1,21 +1,44 @@
 package com.aliferous.launchaulogy_aliferous;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
 import android.widget.ImageView;
 
 public class HomeActivity extends AppCompatActivity {
 
     ImageView navHome, navYoga, navAilment, navProfile;
+    ConstraintLayout HomeScreen,SplashScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        HomeScreen = findViewById(R.id.homePage);
+        SplashScreen = findViewById(R.id.SplashScreen);
+
+        SplashScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final ObjectAnimator oa1 = ObjectAnimator.ofFloat(HomeScreen, "alpha", 0f, 1f);
+                oa1.setDuration(400);
+                oa1.setInterpolator(new AccelerateDecelerateInterpolator());
+                oa1.start();
+                HomeScreen.setVisibility(View.VISIBLE);
+
+            }
+        });
 
 
 
